d319e557aeff92d27c4b331f19cf412428b35a3a
eazz1337 <jac.grotkowski@gmail.com>
2025-08-31 19:21:46 +0200

    adding players table

---
+++ database-schema/table/dim/dim_players.sql
@@ -0,0 +1,6 @@
+-- Table for storing player information
+CREATE TABLE dim_players (
+    player_id BIGINT PRIMARY KEY,   -- Unique numeric identifier for the player (Primary Key)
+    nickname VARCHAR(24) NOT NULL,  -- The player's public in-game nickname (Cannot be null)
+    registration_date DATE          -- The date the player created their account
+);
